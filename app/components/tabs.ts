2126dd98201f86b7c774944ff099a5e15eef731e
berwyn <berwyn.codeweaver@gmail.com>
2016-01-03 15:52:04 -0700

    Start tabs, flesh out card

---
+++ app/components/tabs.ts
@@ -0,0 +1,56 @@
+import { Component, Input } from 'angular2/core';
+import { NgFor }  from 'angular2/common';
+
+export enum TabType {
+    Standard,
+    Sidebar
+}
+
+@Component({
+    selector: 'pfm-tabs',
+    template: `
+        <ul>
+            <li *ngFor="#tab of tabs" (click)="selectTab(tab)">
+                {{ tab.title }}
+            </li>
+        </ul>
+        <ng-content></ng-content>
+    `,
+    directives: [NgFor]
+})
+export class TabsComponent {
+    
+    @Input()
+    type: TabType = TabType.Standard;
+    tabs: TabComponent[] = [];
+    
+    addTab(tab: TabComponent) {
+        if(this.tabs.length === 0) {
+            tab.active = true;
+        }
+        this.tabs.push(tab);
+    }
+    
+    selectTab(tab: TabComponent) {
+        this.tabs.forEach(t => t.active = (t === tab))
+    }
+}
+
+@Component({
+    selector: 'pfm-tab',
+    template: `
+        <div [hidden]="!active">
+            <ng-content></ng-content>
+        </div>
+    `
+})
+export class TabComponent {
+    
+    @Input()
+    title: string;
+    active: boolean = false;
+    
+    constructor(tabs: TabsComponent) {
+        tabs.addTab(this);
+    }
+}
--- app/components/trackGrid.ts
+++ app/components/trackGrid.ts
@@ -1,18 +1,15 @@
 import { Component, Optional, Input } from 'angular2/core';
 import { NgFor } from 'angular2/common';
+import { PfmCard } from './card';
 import { Track } from '../models/track';
 
 @Component({
     selector: 'pfm-track-grid',
     template: `
         <div class="pfm-track-grid">
-            <div *ngFor="#track of tracks">
-                <div class="pfm-track-art">
-                    <img [src]="loadCover(track)" />
-                </div>
-                <h1>{{ track.title }}</h1>
-                <span>{{ track.stats.plays }} Plays</span>
-            </div>
+            <pfm-card *ngFor="#track of tracks" [title]="track.title">
+                <img image [src]="loadCover(track)" />
+            </pfm-card>
         </div>
     `,
     styles: [`
@@ -37,7 +34,7 @@ import { Track } from '../models/track';
             vertical-align: middle;
         }
     `],
-    directives: [NgFor]
+    directives: [NgFor, PfmCard]
 })
 export class TrackGridComponent{
     
